10db71a128b41ba1af811f9a714858db9ee42995
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2025-02-26 23:55:29 +0300

    Object creation rework
    Added interactions
    Changed inview decorator

--- engine/src/HTMLWindow/RunInView.ts
+++ engine/src/HTMLWindow/RunInView.ts
@@ -3,18 +3,27 @@ import { HTMLObject } from "engine/Objects/HTMLObject.ts";
 import { Engine } from "engine/Engine.ts";
 
 
-export function inview<T extends HTMLObject>(target: (this: T | any, ...args: any[]) => any, _ctx: ClassMethodDecoratorContext) {
-    return function (this: T | any, ...args: any[]) {
-        const scriptText = target.toString();
+
+export function getInviewFunction<ARGS extends any[]>(target: (this: any, ...args: ARGS) => any) {
+    return function (this: any, ...args: ARGS) {
+        let scriptText = target.toString();
         const argsText = scriptText.match(/\(([^)]+)\)/)?.[0] ?? "()";
         const argsNames = argsText.split(",").map(arg => arg.trim().replaceAll("(", "").replaceAll(")", ""));
         let filledArguments = argsText;
         for (const arg of argsNames) {
             filledArguments = filledArguments.replaceAll(arg, JSON.stringify(args.shift()));
         }
-        const script = `(function ${scriptText})${filledArguments}`;
 
-        if (this instanceof HTMLObject) {
+        // Handle arrow functions
+        // TODO: rewrite. Now ruins any inner arrow functions
+        if (scriptText.includes("=>")) {
+            scriptText = scriptText.replace("=>", "{") + "}";
+        }
+        
+        const script = `(${scriptText.includes("function") ? "" : "function "}${scriptText})${filledArguments}`;
+        // console.log(script);
+
+        if (this && this instanceof HTMLObject) {
             this.exec(script);
         } else {
             Engine.instance.DOM.runScript(script);
---
+++ engine/src/engine/Objects/ObjectInteractions.ts
@@ -0,0 +1,41 @@
+import { Logger } from "misc/Logger.ts";
+import { getInviewFunction } from "window/RunInView.ts";
+
+export enum InteractionType {
+    RUN_CODE = "runCode"
+}
+
+interface BaseInteraction {
+    type: InteractionType;
+}
+
+interface RunCodeInteraction extends BaseInteraction {
+    type: InteractionType.RUN_CODE;
+    code: string;
+    inview: boolean;
+}
+
+export type Interaction = RunCodeInteraction;
+
+
+const logger = new Logger(new (class ObjectInteractionParser {})());
+
+
+export function parseInteraction(interaction: Interaction | Interaction[]): () => void {
+    if (Array.isArray(interaction)) {
+        return () => {
+            interaction.map(parseInteraction).forEach(f => f())
+        };
+    }
+
+    switch (interaction.type) {
+        case InteractionType.RUN_CODE:
+            return () => {
+                interaction.inview ? getInviewFunction((text) => eval(text))(interaction.code) : eval(interaction.code);
+            }
+
+        default:
+            logger.error("Unknown interaction type: ", interaction.type);
+            return () => {};
+    }
+}
