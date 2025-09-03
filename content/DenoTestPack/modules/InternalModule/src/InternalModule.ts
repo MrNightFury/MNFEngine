d2b26c0af02fa50cab160407ded891284bdeba48
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2025-09-04 02:09:53 +0300

    Reworked denotestpack's player

--- content/DenoTestPack/modules/InternalModule/src/InternalModule.ts
+++ content/DenoTestPack/modules/InternalModule/src/InternalModule.ts
@@ -1,10 +1,13 @@
 import { Module } from "engine/Modules/Module.ts";
 import { Player } from "./Player.ts";
+import { Engine } from "engine/Engine.ts";
 
 
 export class InternalModule extends Module {
     name = "InternalModule";
 
+    core = Engine.instance.modulesManager.getModule("CoreModule")!;
+    
     // deno-lint-ignore require-await
     async load() {
         this.engine.registry.objectClasses.register("player", Player);
--- content/DenoTestPack/modules/InternalModule/src/Player.ts
+++ content/DenoTestPack/modules/InternalModule/src/Player.ts
@@ -1,24 +1,37 @@
-import { SceneObject } from "engine/Objects/SceneObject.ts";
+import { Position, SceneObject } from "engine/Objects/SceneObject.ts";
 import { Engine } from "engine/Engine.ts";
 import { InternalModule } from "./mod.ts";
 import type { CoreModule } from "modules/CoreModule/src/mod.ts";
 import { inview } from "window/RunInView.ts";
+import { notify } from "engine/Components/WatchDecorator.ts";
+
 
 const coreModule = Engine.instance.modulesManager.getModule<CoreModule>("CoreModule")!;
 const controls = coreModule.controls;
 const KeyState = coreModule.exports.KeyState;
 
+
 export class Player extends SceneObject {
     module = Engine.instance.modulesManager.getModule<InternalModule>("InternalModule")!;
+    
 
     @inview
-    move(x: number, y: number) {
+    moveInview(x: number, y: number) {
         x *= 2; y *= 2;
         this.$.css({ bottom: `+=${y}px`, left: `+=${x}px` });
     }
 
+    move(x: number, y: number) {
+        this.moveInview(x, y);
+        this.position = { x: this.position.x + x, y: this.position.y + y };
+    }
+
     constructor() {
+        console.log("Player constructor");
         super("p", { x: 100, y: 100 }, { style: "color: red" });
+        this.addComponent(new coreModule.exports.ImageComponent(this, {
+            src: "https://placehold.net/400x400.png"
+        }))
 
         Engine.instance.DOM.emitter.on("pageLoaded", () => {
             this.exec(() => {
